259ab13449f9dac4d1e9cd6c75e05e9b66440e33
Dewald <dewald@perfectcircle.co.za>
2024-10-23 15:14:29 +0200

    pdf gen

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
-Rails.application.config.assets.precompile += %w( bootstrap.min.css pdf.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css pdf.css ../fonts/glyphicons-halflings-regular.eot )
 Rails.application.config.assets.precompile += ['formatting.css']
