fc8cdb1c32ea1c341eea7b7601a870b64cb5a255
Dewald <dewald@perfectcircle.co.za>
2024-10-23 14:56:41 +0200

    pdf gen

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Jetcrew360::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.precompile += ['*.js', '*.json', '*.css', '*.scss', 'refinery/*.js', 'refinery/*.css', "/fonts/glyphicons-halflings-regular.eot"]
+  config.assets.precompile += ['*.js', '*.json', '*.css', '*.scss', 'refinery/*.js', 'refinery/*.css']
 
   # Generate digests for assets URLs
   config.assets.digest = true
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
 Rails.application.config.assets.precompile += ['formatting.css']
+Rails.application.config.assets.precompile += ['../fonts/glyphicons-halflings-regular.eot']
