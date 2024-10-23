5f9a5d9975eeafe26ab47dc5e76051a9b2ac7cef
Dewald <dewald@perfectcircle.co.za>
2024-10-23 15:15:12 +0200

    pdf gen

--- public/assets/jquery.colorbox-fi.js
+++
@@ -1,1 +0,0 @@
-jQuery.extend(jQuery.colorbox.settings,{current:"Kuva {current} / {total}",previous:"Edellinen",next:"Seuraava",close:"Sulje",xhrError:"Sis\xe4ll\xf6n lataaminen ep\xe4onnistui.",imgError:"Kuvan lataaminen ep\xe4onnistui.",slideshowStart:"Aloita kuvaesitys.",slideshowStop:"Lopeta kuvaesitys."});
